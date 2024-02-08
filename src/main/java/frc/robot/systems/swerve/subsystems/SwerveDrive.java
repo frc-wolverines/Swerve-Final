ce64168569d9391be858f2c9f34f1006b2ffd698
frc-wolverines <rosemont5274@gmail.com>
2024-02-07 21:43:56 -0800

    removed unused import

--- src/main/java/frc/robot/systems/swerve/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/systems/swerve/subsystems/SwerveDrive.java
@@ -7,7 +7,6 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.montylib.swerve.DriveFrame;
