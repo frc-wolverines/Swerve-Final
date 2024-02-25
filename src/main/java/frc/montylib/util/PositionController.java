bc8f4f05fe0146e45c37c02a9167799aeffab18e
frc-wolverines <rosemont5274@gmail.com>
2024-02-24 16:27:01 -0800

    commented PositionController.java

--- src/main/java/frc/montylib/util/PositionController.java
+++ src/main/java/frc/montylib/util/PositionController.java
@@ -6,13 +6,23 @@ import edu.wpi.first.math.controller.PIDController;
 
 public class PositionController {
 
-    public double toPowerPercantage(double target_position) {
+    /**
+     * Converts a target position to a power value based on configured values
+     * @param target_position the target position 
+     * @return a power value related to the position delta in decimal percentage
+     */
+    public double calculate(double target_position) {
         return controller.calculate(feedbackSupplier.get(), target_position);
     }
 
     private PIDController controller;
     private Supplier<Double> feedbackSupplier;
 
+    /**
+     * Construcs a new instance of PositionController
+     * @param constants the PIDConstants for an integrated PIDController
+     * @param current_position_supplier a function to return the current position of the mechanism
+     */
     public PositionController(PIDConstants constants, Supplier<Double> current_position_supplier) {
         this.controller = constants.toPIDController();
         this.feedbackSupplier = current_position_supplier;
